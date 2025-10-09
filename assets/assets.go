acc3cb238e4c16f03a031a216623339f2dc12012
Sam Redway <sam.redway@outlook.com>
2025-10-09 23:08:28 +0100

    add logic for loading from file

--- assets/assets.go
+++ assets/assets.go
@@ -1,13 +1,17 @@
 package assets
 
 import (
-	"fmt"
-
 	"github.com/hajimehoshi/ebiten/v2"
+	"image"
+	_ "image/jpeg"
+	_ "image/png"
+	"os"
 )
 
 type Assets struct {
-	imgs map[string]*ebiten.Image
+	imgs         map[string]*ebiten.Image
+	tileMaps     map[string][]*ebiten.Image
+	spriteSheets map[string][]*ebiten.Image
 }
 
 func NewAssets() *Assets {
@@ -18,11 +22,42 @@ func NewAssets() *Assets {
 func (a *Assets) GetImage(imgName string) (*ebiten.Image, error) {
 	img, ok := a.imgs[imgName]
 	if !ok {
-		return nil, fmt.Errorf("no image with name %q", imgName)
+		panic("no image with name " + imgName)
 	}
 	return img, nil
 }
 
 func (a *Assets) AddImage(imgName string, img *ebiten.Image) {
 	a.imgs[imgName] = img
+}
+
+func (a *Assets) LoadTileSetFromPath(name, path string, tileSize int) {
+	sheet := loadEbitenImage(path)
+	b := sheet.Bounds()
+	w := b.Dx()
+	h := b.Dy()
+
+	var tiles []*ebiten.Image
+	for y := 0; y < h; y += tileSize {
+		for x := 0; x < w; x += tileSize {
+			sub := sheet.SubImage(image.Rect(x, y, x+tileSize, y+tileSize)).(*ebiten.Image)
+			tiles = append(tiles, sub)
+		}
+	}
+	a.tileMaps[name] = tiles
+}
+
+func loadEbitenImage(path string) *ebiten.Image {
+	f, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	img, _, err := image.Decode(f)
+	if err != nil {
+		panic(err)
+	}
+
+	return ebiten.NewImageFromImage(img)
 }
